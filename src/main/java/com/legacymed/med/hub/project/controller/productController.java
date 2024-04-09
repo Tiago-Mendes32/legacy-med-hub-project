9cd2559780a266c42041e6c5643049f4eb4bb6cc
Tiago-Mendes32 <tiago.mendes.goes@gmail.com>
2024-04-09 11:13:20 -0300

    product controler create

--- src/main/java/com/legacymed/med/hub/project/controller/productController.java
+++ src/main/java/com/legacymed/med/hub/project/controller/productController.java
@@ -1,8 +1,15 @@
 package com.legacymed.med.hub.project.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/products")
 public class productController {
 
+	@PostMapping
+	public ProductDetailsDTO insertProduct(NewProductData product) {
+		System.out.println("Usuário cadastrado");
+	}
 }
