5c2cce7687bd008242916c843f80946c4656e43b
Tiago-Mendes32 <tiago.mendes.goes@gmail.com>
2024-04-12 11:56:56 -0300

    MethodArgumentNotValidException create and tratment

--- src/main/java/com/legacymed/med/hub/project/config/RequestHandlerManagerExceptions.java
+++ src/main/java/com/legacymed/med/hub/project/config/RequestHandlerManagerExceptions.java
@@ -1,6 +1,12 @@
 package com.legacymed.med.hub.project.config;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,8 +15,19 @@ import jakarta.persistence.EntityNotFoundException;
 @RestControllerAdvice
 public class RequestHandlerManagerExceptions {
 
-	 @ExceptionHandler(EntityNotFoundException.class)
-	    public ResponseEntity<Void> entityNotFoundException() {
-	        return ResponseEntity.notFound().build();
-	    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> entityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+        Map<String, String> errorMap = errors.stream()
+                                            .collect(Collectors.toMap(
+                                                FieldError::getField,
+                                                FieldError::getDefaultMessage
+                                            ));
+        return ResponseEntity.badRequest().body(errorMap);
+    }
 }
